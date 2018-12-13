4ba5e8799465a946a0c435aeb877ace9511b81d7
buitim <buitim@oregonstate.edu>
2018-12-13 01:58:29 -0800

    added spectrum analysis. facing issues with overflow

--- crowdcontrol.py
+++ crowdcontrol.py
@@ -6,12 +6,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 import time
 from tkinter import TclError
+from scipy.fftpack import fft
 
 # use this backend to display in separate Tk window
 %matplotlib tk
 
 # constants
-CHUNK = 1024 * 4             # samples per frame
+CHUNK = 1024 * 6             # samples per frame
 # Also affects refresh rate and may cause overflow depending on value
 
 FORMAT = pyaudio.paInt16     # audio format (bytes per sample?)
@@ -19,7 +20,7 @@ CHANNELS = 1                 # single channel for microphone
 RATE = 44100                 # samples per second
 
 # create matplotlib figure and axes
-fig, ax = plt.subplots(1, figsize=(15, 7))
+fig, (ax, ax2) = plt.subplots(2, figsize=(15, 7))
 
 # pyaudio class instance
 p = pyaudio.PyAudio()
@@ -36,9 +37,11 @@ stream = p.open(
 
 # variable for plotting
 x = np.arange(0, 2 * CHUNK, 2)
+x_fft = np.linspace(0, RATE, CHUNK)
 
 # create a line object with random data
 line, = ax.plot(x, np.random.rand(CHUNK), '-', lw=2)
+fftLine, = ax2.semilogx(x_fft, np.random.rand(CHUNK), '-', lw=2)
 
 # basic formatting for the axes
 ax.set_title('AUDIO WAVEFORM')
@@ -47,6 +50,8 @@ ax.set_ylabel('volume')
 ax.set_ylim(0, 255)
 ax.set_xlim(0, 2 * CHUNK)
 plt.setp(ax, xticks=[0, CHUNK, 2 * CHUNK], yticks=[0, 128, 255])
+
+ax2.set_xlim(20, RATE / 2)
 
 # show the plot
 plt.show(block=False)
@@ -69,6 +74,9 @@ while True:
     data_np = np.array(data_int, dtype='b')[::2] + 128
 
     line.set_ydata(data_np)
+
+    y_fft = fft(data_int)
+    fftLine.set_ydata(np.abs(y_fft[0:CHUNK]) * 2 / (256 * CHUNK))
 
     # update figure canvas
     try:
