95b9d3043a4df84cb5046b6fbd7301ee9813c2a1
buitim <buitim@oregonstate.edu>
2018-12-13 23:45:35 -0800

    Changed code to work with oop and pyqtgraph

--- crowdcontrol.py
+++ crowdcontrol.py
@@ -1,94 +1,105 @@
-# %%
-import pyaudio
-import os
-import struct
 import numpy as np
-import matplotlib.pyplot as plt
-import time
-from tkinter import TclError
+from pyqtgraph.Qt import QtGui, QtCore
+import pyqtgraph as pg
+
+import struct
+import pyaudio
 from scipy.fftpack import fft
 
-# use this backend to display in separate Tk window
-%matplotlib tk
+import sys
+import time
 
-# constants
-CHUNK = 1024 * 6             # samples per frame
-# Also affects refresh rate and may cause overflow depending on value
 
-FORMAT = pyaudio.paInt16     # audio format (bytes per sample?)
-CHANNELS = 1                 # single channel for microphone
-RATE = 44100                 # samples per second
+class AudioStream(object):
+    def __init__(self):
 
-# create matplotlib figure and axes
-fig, (ax, ax2) = plt.subplots(2, figsize=(15, 7))
+        # pyqtgraph stuff
+        pg.setConfigOptions(antialias=True)
+        self.traces = dict()
+        self.app = QtGui.QApplication(sys.argv)
+        self.win = pg.GraphicsWindow(title='Spectrum Analyzer')
+        self.win.setWindowTitle('Spectrum Analyzer')
+        self.win.setGeometry(5, 115, 1910, 1070)
 
-# pyaudio class instance
-p = pyaudio.PyAudio()
+        wf_xlabels = [(0, '0'), (2048, '2048'), (4096, '4096')]
+        wf_xaxis = pg.AxisItem(orientation='bottom')
+        wf_xaxis.setTicks([wf_xlabels])
 
-# stream object to get data from microphone
-stream = p.open(
-    format=FORMAT,
-    channels=CHANNELS,
-    rate=RATE,
-    input=True,
-    output=True,
-    frames_per_buffer=CHUNK
-)
+        wf_ylabels = [(0, '0'), (127, '128'), (255, '255')]
+        wf_yaxis = pg.AxisItem(orientation='left')
+        wf_yaxis.setTicks([wf_ylabels])
 
-# variable for plotting
-x = np.arange(0, 2 * CHUNK, 2)
-x_fft = np.linspace(0, RATE, CHUNK)
+        sp_xlabels = [
+            (np.log10(10), '10'), (np.log10(100), '100'),
+            (np.log10(1000), '1000'), (np.log10(22050), '22050')
+        ]
+        sp_xaxis = pg.AxisItem(orientation='bottom')
+        sp_xaxis.setTicks([sp_xlabels])
 
-# create a line object with random data
-line, = ax.plot(x, np.random.rand(CHUNK), '-', lw=2)
-fftLine, = ax2.semilogx(x_fft, np.random.rand(CHUNK), '-', lw=2)
+        self.waveform = self.win.addPlot(
+            title='WAVEFORM', row=1, col=1, axisItems={'bottom': wf_xaxis, 'left': wf_yaxis},
+        )
+        self.spectrum = self.win.addPlot(
+            title='SPECTRUM', row=2, col=1, axisItems={'bottom': sp_xaxis},
+        )
 
-# basic formatting for the axes
-ax.set_title('AUDIO WAVEFORM')
-ax.set_xlabel('samples')
-ax.set_ylabel('volume')
-ax.set_ylim(0, 255)
-ax.set_xlim(0, 2 * CHUNK)
-plt.setp(ax, xticks=[0, CHUNK, 2 * CHUNK], yticks=[0, 128, 255])
+        # pyaudio stuff
+        self.FORMAT = pyaudio.paInt16
+        self.CHANNELS = 1
+        self.RATE = 44100
+        self.CHUNK = 1024 * 2
 
-ax2.set_xlim(20, RATE / 2)
+        self.p = pyaudio.PyAudio()
+        self.stream = self.p.open(
+            format=self.FORMAT,
+            channels=self.CHANNELS,
+            rate=self.RATE,
+            input=True,
+            output=True,
+            frames_per_buffer=self.CHUNK,
+        )
+        # waveform and spectrum x points
+        self.x = np.arange(0, 2 * self.CHUNK, 2)
+        self.f = np.linspace(0, self.RATE / 2, self.CHUNK / 2)
 
-# show the plot
-plt.show(block=False)
+    def start(self):
+        if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
+            QtGui.QApplication.instance().exec_()
 
-print('stream started')
+    def set_plotdata(self, name, data_x, data_y):
+        if name in self.traces:
+            self.traces[name].setData(data_x, data_y)
+        else:
+            if name == 'waveform':
+                self.traces[name] = self.waveform.plot(pen='c', width=3)
+                self.waveform.setYRange(0, 255, padding=0)
+                self.waveform.setXRange(0, 2 * self.CHUNK, padding=0.005)
+            if name == 'spectrum':
+                self.traces[name] = self.spectrum.plot(pen='m', width=3)
+                self.spectrum.setLogMode(x=True, y=True)
+                self.spectrum.setYRange(-4, 0, padding=0)
+                self.spectrum.setXRange(
+                    np.log10(20), np.log10(self.RATE / 2), padding=0.005)
 
-# for measuring frame rate
-frame_count = 0
-start_time = time.time()
+    def update(self):
+        wf_data = self.stream.read(self.CHUNK)
+        wf_data = struct.unpack(str(2 * self.CHUNK) + 'B', wf_data)
+        wf_data = np.array(wf_data, dtype='b')[::2] + 128
+        self.set_plotdata(name='waveform', data_x=self.x, data_y=wf_data,)
 
-while True:
+        sp_data = fft(np.array(wf_data, dtype='int8') - 128)
+        sp_data = np.abs(sp_data[0:int(self.CHUNK / 2)]
+                         ) * 2 / (128 * self.CHUNK)
+        self.set_plotdata(name='spectrum', data_x=self.f, data_y=sp_data)
 
-    # binary data
-    data = stream.read(CHUNK)
+    def animation(self):
+        timer = QtCore.QTimer()
+        timer.timeout.connect(self.update)
+        timer.start(20)
+        self.start()
 
-    # convert data to integers, make np array, then offset it by 127
-    data_int = struct.unpack(str(2 * CHUNK) + 'B', data)
 
-    # create np array and offset by 128
-    data_np = np.array(data_int, dtype='b')[::2] + 128
+if __name__ == '__main__':
 
-    line.set_ydata(data_np)
-
-    y_fft = fft(data_int)
-    fftLine.set_ydata(np.abs(y_fft[0:CHUNK]) * 2 / (256 * CHUNK))
-
-    # update figure canvas
-    try:
-        fig.canvas.draw()
-        fig.canvas.flush_events()
-        frame_count += 1
-
-    except TclError:
-
-        # calculate average frame rate
-        frame_rate = frame_count / (time.time() - start_time)
-
-        print('stream stopped')
-        print('average frame rate = {:.0f} FPS'.format(frame_rate))
-        break
+    audio_app = AudioStream()
+    audio_app.animation()
